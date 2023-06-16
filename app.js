ff4580a3898ea5fbe8bca11010789ff7471d2e44
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2023-06-16 12:36:02 +0100

    fix: fixed too busy usage

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.disable("x-powered-by");
-if(toobusy_js){
+if(toobusy_js()){
 	apiError.badRequest("Server is busy.")
 }
 
